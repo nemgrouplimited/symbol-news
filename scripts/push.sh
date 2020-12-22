64941d690b1ec8f9185ac84a99e46ed463766474
Bassem <bassem.magdyy94@gmail.com>
2020-12-22 12:16:01 +0200

    update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -8,7 +8,7 @@ setup_git() {
 commit_website_files() {
   git checkout -b gh-pages
   git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" --allow-empty
 }
 
 upload_files() {
